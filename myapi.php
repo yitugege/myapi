131f3767c487bdfc75ddeeb9ac8caa3b35a274ea
yitu <sam.zxj@qq.com>
2022-10-04 12:30:32 -0500

    myapi.php

--- myapi.php
+++ myapi.php
@@ -24,131 +24,194 @@
 /**
  * Disable direct access.
  */
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
-class Slug_Custom_Route extends WP_REST_Controller
+class WC_REST_Custom_Controller
 {
-
     /**
-     * Register the routes for the objects of the controller.
+     * You can extend this class with
+     * WP_REST_Controller / WC_REST_Controller / WC_REST_Products_V2_Controller / WC_REST_CRUD_Controller etc.
+     * Found in packages/woocommerce-rest-api/src/Controllers/
      */
+    protected $namespace = 'wc/v3';
 
-    public function __construct()
+    protected $rest_base = 'custom';
+
+    public function get_custom($data)
     {
-        $this->namespace    = 'yitu/v1';
-        $this->rest_base = 'products';
-        add_action('rest_api_init', array($this, 'register_routes'));
+        return array('custom' => 'Data');
     }
-
-    // public function register_routes(){
-    //     register_rest_route($this->namespace,'/' . $this->rest_base,array(
-    //         'methods' => 'GET',
-    //         'callback' => array($this, 'myapi_post' )
-    //     ));
-    // }
 
     public function register_routes()
     {
-        register_rest_route($this->namespace, '/' . $this->rest_base, array(
+        register_rest_route(
+            $this->namespace,
+            '/' . $this->rest_base,
             array(
-                'methods'             => WP_REST_Server::READABLE,
-                'callback'            => array($this, 'myapi_post'),
-                # 'permission_callback' => array( $this, 'get_items_permissions_check' ),
-                'args'                => array(),
-            ),
-            array(
-                'methods'             => WP_REST_Server::CREATABLE,
-                'callback'            => array($this, 'create_item'),
-                #  'permission_callback' => array( $this, 'create_item_permissions_check' ),
-                'args'                => $this->get_endpoint_args_for_item_schema(true),
-            ),
-        ));
-        register_rest_route($this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)', array(
-            array(
-                'methods'             => WP_REST_Server::READABLE,
-                'callback'            => array($this, 'get_items'),
-                #  'permission_callback' => array( $this, 'get_item_permissions_check' ),
-                'args'                => array(
-                    'context' => array(
-                        'default' => 'view',
-                    ),
-                ),
-            ),
-            array(
-                'methods'             => WP_REST_Server::EDITABLE,
-                'callback'            => array($this, 'update_item'),
-                'permission_callback' => array($this, 'update_item_permissions_check'),
-                'args'                => $this->get_endpoint_args_for_item_schema(false),
-            ),
-            array(
-                'methods'             => WP_REST_Server::DELETABLE,
-                'callback'            => array($this, 'delete_item'),
-                'permission_callback' => array($this, 'delete_item_permissions_check'),
-                'args'                => array(
-                    'force' => array(
-                        'default' => false,
-                    ),
-                ),
-            ),
-        ));
-        register_rest_route($this->namespace, '/' . $this->rest_base . '/schema', array(
-            'methods'  => WP_REST_Server::READABLE,
-            'callback' => array($this, 'get_public_item_schema'),
-        ));
-    }
-
-
-    public function myapi_post()
-    {
-        return 'i am working';
-    }
-
-
-    public function get_items($request)
-    {
-        // In practice this function would fetch the desired data. Here we are just making stuff up.
-        $products = array(
-            '1' => 'I am product 1',
-            '2' => 'I am product 2',
-            '3' => 'I am product 3',
+                'methods' => 'GET',
+                'callback' => array($this, 'get_custom'),
+                'permission_callback' => array($this, 'get_items_permissions_check'),
+            )
         );
-
-        // Here we are grabbing the 'id' path variable from the $request object. WP_REST_Request implements ArrayAccess, which allows us to grab properties as though it is an array.
-        $id = (string) $request['id'];
-
-
-        if (isset($products[$id])) {
-            // Grab the product.
-            $product = $products[$id];
-
-            // Return the product as a response.
-            return rest_ensure_response($product);
-        } else {
-            // Return a WP_Error because the request product was not found. In this case we return a 404 because the main resource was not found.
-            return new WP_Error('rest_product_invalid', esc_html__('The product does not exist.', 'my-text-domain'), array('status' => 404));
+    }
+    public function get_items_permissions_check($request)
+    {
+        if (!wc_rest_check_manager_permissions('settings', 'read')) {
+            return new WP_Error('woocommerce_rest_cannot_view', __('Sorry, you cannot list resources.', 'woocommerce'), array('status' => rest_authorization_required_code()));
         }
 
-        // If the code somehow executes to here something bad happened return a 500.
-        return new WP_Error('rest_api_sad', esc_html__('Something went horribly wrong.', 'my-text-domain'), array('status' => 500));
+        return true;
     }
 }
 
+add_filter('woocommerce_rest_api_get_rest_namespaces', 'woo_custom_api');
 
-if (is_plugin_active('myapi/myapi.php') ) {
-	yitu_rest_api_v1_load_plugin('Slug_Custom_Route');
+function woo_custom_api($controllers)
+{
+    $controllers['wc/v3']['custom'] = 'WC_REST_Custom_Controller';
+
+    return $controllers;
 }
 
 
-function yitu_rest_api_v1_load_plugin($class_name){
-    global $yitu_rest_api_v1_plgins;
-    if (!isset ($yitu_rest_api_v1_plgins)){
-        $yitu_rest_api_v1_plgins = array();
-        $_GLOBALS['yitu_rest_api_v1_plugins'] =array();
-    }
+// class Slug_Custom_Route extends WP_REST_Controller
+// {
 
-    if (!isset($yitu_rest_api_v1_plgins[$class_name])){
-        $yitu_rest_api_v1_plgins[$class_name]= new $class_name();
-    }
-}
+//     /**
+//      * Register the routes for the objects of the controller.
+//      */
+
+//     public function __construct()
+//     {
+//         $this->namespace    = 'yitu/v1';
+//         $this->rest_base = 'products';
+//         add_action('rest_api_init', array($this, 'register_routes'));
+//     }
+
+//     // public function register_routes(){
+//     //     register_rest_route($this->namespace,'/' . $this->rest_base,array(
+//     //         'methods' => 'GET',
+//     //         'callback' => array($this, 'myapi_post' )
+//     //     ));
+//     // }
+    
+//     public function register_routes()
+//     {
+//         register_rest_route($this->namespace, '/' . $this->rest_base, array(
+//             array(
+//                 'methods'             => WP_REST_Server::READABLE,
+//                 'callback'            => array($this, 'myapi_post'),
+//                 'permission_callback' => array( $this, 'get_items_permissions_check' ),
+//                 'args'                => array(),
+//             ),
+//             array(
+//                 'methods'             => WP_REST_Server::CREATABLE,
+//                 'callback'            => array($this, 'create_item'),
+                
+//                 'args'                => $this->get_endpoint_args_for_item_schema(true),
+//             ),
+//         ));
+//         register_rest_route($this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)', array(
+//             array(
+//                 'methods'             => WP_REST_Server::READABLE,
+//                 'callback'            => array($this, 'get_items'),
+                
+//                 'args'                => array(
+//                     'context' => array(
+//                         'default' => 'view',
+//                     ),
+//                 ),
+//             ),
+//             array(
+//                 'methods'             => WP_REST_Server::EDITABLE,
+//                 'callback'            => array($this, 'update_item'),
+//                 'permission_callback' => array($this, 'update_item_permissions_check'),
+//                 'args'                => $this->get_endpoint_args_for_item_schema(false),
+//             ),
+//             array(
+//                 'methods'             => WP_REST_Server::DELETABLE,
+//                 'callback'            => array($this, 'delete_item'),
+//                 'permission_callback' => array($this, 'delete_item_permissions_check'),
+//                 'args'                => array(
+//                     'force' => array(
+//                         'default' => false,
+//                     ),
+//                 ),
+//             ),
+//         ));
+//         register_rest_route($this->namespace, '/' . $this->rest_base . '/schema', array(
+//             'methods'  => WP_REST_Server::READABLE,
+//             'callback' => array($this, 'get_public_item_schema'),
+//         ));
+//     }
+
+
+//     public function myapi_post()
+//     {
+//         return 'i am working';
+//     }
+
+//     	/**
+// 	 * Check whether a given request has permission to read site data.
+// 	 *
+// 	 * @param  WP_REST_Request $request Full details about the request.
+// 	 * @return WP_Error|boolean
+// 	 */
+// 	public function get_items_permissions_check( $request ) {
+// 		if ( ! wc_rest_check_manager_permissions( 'settings', 'read' ) ) {
+//             print_r($request);
+// 			return new WP_Error( 'woocommerce_rest_cannot_view', __( 'Sorry, you cannot list resources.', 'woocommerce' ), array( 'status' => rest_authorization_required_code() ) );
+// 		}
+        
+// 		return true;
+// 	}
+
+//     public function get_items($request)
+//     {
+     
+//         // In practice this function would fetch the desired data. Here we are just making stuff up.
+//         $products = array(
+//             '1' => 'I am product 1',
+//             '2' => 'I am product 2',
+//             '3' => 'I am product 3',
+//         );
+
+//         // Here we are grabbing the 'id' path variable from the $request object. WP_REST_Request implements ArrayAccess, which allows us to grab properties as though it is an array.
+//         $id = (string) $request['id'];
+
+
+//         if (isset($products[$id])) {
+//             // Grab the product.
+//             $product = $products[$id];
+
+//             // Return the product as a response.
+//             return rest_ensure_response($product);
+//         } else {
+//             // Return a WP_Error because the request product was not found. In this case we return a 404 because the main resource was not found.
+//             return new WP_Error('rest_product_invalid', esc_html__('The product does not exist.', 'my-text-domain'), array('status' => 404));
+//         }
+
+//         // If the code somehow executes to here something bad happened return a 500.
+//         return new WP_Error('rest_api_sad', esc_html__('Something went horribly wrong.', 'my-text-domain'), array('status' => 500));
+//     }
+// }
+
+
+// if (is_plugin_active('myapi/myapi.php') ) {
+// 	yitu_rest_api_v1_load_plugin('Slug_Custom_Route');
+// }
+
+
+// function yitu_rest_api_v1_load_plugin($class_name){
+//     global $yitu_rest_api_v1_plgins;
+//     if (!isset ($yitu_rest_api_v1_plgins)){
+//         $yitu_rest_api_v1_plgins = array();
+//         $_GLOBALS['yitu_rest_api_v1_plugins'] =array();
+//     }
+
+//     if (!isset($yitu_rest_api_v1_plgins[$class_name])){
+//         $yitu_rest_api_v1_plgins[$class_name]= new $class_name();
+//     }
+// }
